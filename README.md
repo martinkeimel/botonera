94da916cbac9a3825c16bd52fd328ecd1cafda90
martinkeimel <martinkeimel@gmail.com>
2017-03-08 19:46:50 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+botonera
+==================
+
+#### To run this example: 
+1. `npm install`
+2. `bower install`
+
+[![Deploy](https://www.herokucdn.com/deploy/button.png)](https://heroku.com/deploy)
+
+
---
+++ public/app.js
@@ -0,0 +1,40 @@
+'use strict';
+
+
+// Declare app level module which depends on filters, and services
+var app = angular.module('botoneraApp', []);
+
+app.controller('botoneraCtrl', function($scope) {
+    $scope.botones = [
+        {
+            image: 'images/btn_blue.png',
+            text: 'BABOOM!',
+            audio: 'media/baboom.mp3'
+        },
+        {
+            image: 'images/btn_red.png',
+            text: 'Buenos dias',
+            audio: 'media/buenos dias.mp3'
+        },
+        {
+            image: 'images/btn_orange.png',
+            text: 'Que placer',
+            audio: 'media/que placer.mp3'
+        }
+    ];
+    
+    $scope.bonus = [
+        {
+            image: 'images/btn_green.png',
+            text: 'Bonus',
+            audio: 'media/fefo come hombres.mp3'
+        }
+    ];
+    
+    $scope.playAudio = function (audioUrl){
+        var audio = new Audio(audioUrl);
+        audio.play();
+    }
+});
+
+
